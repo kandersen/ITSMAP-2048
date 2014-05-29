e518443b5f6b91d42ca5de37b85c70e98fd3dbf6
kandersen <kja@cs.au.dk>
2014-05-29 17:00:02 +0200

    prettified grid component, keeping it square

--- Twentyfortyeight/src/dk/compsci/kja/twentyfortyeight/TwentyfortyeightGrid.java
+++ Twentyfortyeight/src/dk/compsci/kja/twentyfortyeight/TwentyfortyeightGrid.java
@@ -9,6 +9,7 @@ import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class TwentyfortyeightGrid extends View {
 
@@ -22,6 +23,7 @@ public class TwentyfortyeightGrid extends View {
 	
 	private int[] tiles;
 	private RectF gridBackground;
+	private RectF cell;
 	
 	public TwentyfortyeightGrid(Context context) {  
 		super(context);
@@ -36,15 +38,18 @@ public class TwentyfortyeightGrid extends View {
 	private void init() {
 		backgroundColor = new Paint();
 		backgroundColor.setColor(Color.parseColor("#FAF8EF"));
-		backgroundColor.setStyle(Style.FILL);
-			
+		backgroundColor.setStyle(Style.FILL);			
 		background = new Rect(0, 0, 0, 0);
 		
 		gridBackgroundColor = new Paint();
 		gridBackgroundColor.setColor(Color.parseColor("#BBADA0"));
+		gridBackgroundColor.setStyle(Style.FILL);
+		gridBackground = new RectF(0, 0, 0, 0);
 		
 		emptyCellColor = new Paint();
 		emptyCellColor.setColor(Color.rgb(238, 228, 218));
+		
+		cell = new RectF(0, 0, 0, 0);
 		
 		tileColors = new Paint[11];
 		for (int i = 0; i < tileColors.length; ++i) {
@@ -56,17 +61,45 @@ public class TwentyfortyeightGrid extends View {
 		tileColors[3].setColor(Color.parseColor("#F2B179"));
 		
 		tiles = new int[16];
+		tiles[3] = 2;
 		
+	}
+	
+	
+
+	@Override
+	protected void onDraw(Canvas canvas) {	
+		int size = getWidth();
+		background.set(0, 0, size, size);
+		canvas.drawRect(background, backgroundColor);
+		
+		gridBackground.set(0, 0, size, size);
+		canvas.drawRoundRect(gridBackground, 10.0f, 10.0f, gridBackgroundColor);
+		
+		int margin = size / 32;
+		int cellSize = (size - 5 * margin) / 4;
+		cell.set(margin, margin, margin + cellSize, margin + cellSize);
+	
+		for (int i = 0; i < 16; i++) {
+			int topLeftX = margin + (i % 4) * (margin + cellSize);
+			int topLeftY = margin + (i / 4) * (margin + cellSize);
+			cell.set(topLeftX, topLeftY, topLeftX + cellSize, topLeftY + cellSize);
+			canvas.drawRoundRect(cell, 10.0f, 10.0f, emptyCellColor);
+			if (tiles[i] != 0) {
+				canvas.drawRoundRect(cell, 10.0f, 10.0f, tileColors[i]);
+			}
+
+		}
 	}
 
 	@Override
-	protected void onDraw(Canvas canvas) {			
-		background.set(0, 0, getWidth(), getHeight());
-		canvas.drawRect(background, backgroundColor);
-		
-		int gridLength = Math.min(getWidth(), getHeight()) - 2 * GRID_FRAME_MARGIN;
-		background.set(GRID_FRAME_MARGIN, getHeight() - gridLength, gridLength + GRID_FRAME_MARGIN, getHeight() - GRID_FRAME_MARGIN);
-		gridBackground = new RectF(background);
-		canvas.drawRoundRect(gridBackground, 10.0f, 10.0f, gridBackgroundColor);
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int width = MeasureSpec.getSize(widthMeasureSpec);
+		int height = MeasureSpec.getSize(heightMeasureSpec);		
+		int size = Math.min(width, height); 				
+		int squareMeasure = MeasureSpec.makeMeasureSpec(size, MeasureSpec.EXACTLY);
+		super.onMeasure(squareMeasure, squareMeasure);
 	}
+	
+	
 }
