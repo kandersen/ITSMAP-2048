746d81d3f8ceb9e93ab8109f5e8dbce0aea4cd1d
kandersen <kja@cs.au.dk>
2014-05-30 12:34:36 +0200

    Semi-working everything-so-far

--- Twentyfortyeight/src/dk/compsci/kja/twentyfortyeight/MainActivity.java
+++ Twentyfortyeight/src/dk/compsci/kja/twentyfortyeight/MainActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+	private Engine _engine;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);		
+		setContentView(R.layout.activity_main);
+		_engine = new MockEngine();
+		TwentyfortyeightGrid grid = (TwentyfortyeightGrid) findViewById(R.id.grid);
+		grid.setEngine(_engine);
 	}
 	
 }
--- Twentyfortyeight/src/dk/compsci/kja/twentyfortyeight/TwentyfortyeightGrid.java
+++ Twentyfortyeight/src/dk/compsci/kja/twentyfortyeight/TwentyfortyeightGrid.java
@@ -7,13 +7,16 @@ import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.support.v4.view.GestureDetectorCompat;
+import android.view.*;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.LinearLayout;
 
-public class TwentyfortyeightGrid extends View {
+public class TwentyfortyeightGrid extends View implements 
+	EngineListener, GestureDetector.OnGestureListener {
 
-	private static final int GRID_FRAME_MARGIN = 10;
 	Paint backgroundColor;
 	Rect background;
 	
@@ -25,17 +28,23 @@ public class TwentyfortyeightGrid extends View {
 	private RectF gridBackground;
 	private RectF cell;
 	
+	private GestureDetectorCompat gestureDetector;
+	private Engine engine;
+	private Context context;
+	
 	public TwentyfortyeightGrid(Context context) {  
 		super(context);
+		context = context;
 		init();
     }
 	
 	public TwentyfortyeightGrid(Context context, AttributeSet attrs) {
-		super(context, attrs);		
+		super(context, attrs);
+		context = context;
 		init();		
 	}
 	
-	private void init() {
+	private void init() {						
 		backgroundColor = new Paint();
 		backgroundColor.setColor(Color.parseColor("#FAF8EF"));
 		backgroundColor.setStyle(Style.FILL);			
@@ -47,7 +56,7 @@ public class TwentyfortyeightGrid extends View {
 		gridBackground = new RectF(0, 0, 0, 0);
 		
 		emptyCellColor = new Paint();
-		emptyCellColor.setColor(Color.rgb(238, 228, 218));
+		emptyCellColor.setColor(Color.argb(90, 238, 228, 218));
 		
 		cell = new RectF(0, 0, 0, 0);
 		
@@ -59,13 +68,19 @@ public class TwentyfortyeightGrid extends View {
 		tileColors[1].setColor(Color.parseColor("#EEE4DA"));
 		tileColors[2].setColor(Color.parseColor("#EDE0C8"));
 		tileColors[3].setColor(Color.parseColor("#F2B179"));
+		tileColors[4].setColor(Color.parseColor("#FFFFFF"));
 		
 		tiles = new int[16];
-		tiles[3] = 2;
-		
+		tiles[3] = 2;	
 	}
 	
-	
+	public void setEngine(Engine e) {
+		if (engine != null) {
+			engine.removeChangeListener(this);
+		}
+		engine = e;
+		engine.addChangeListener(this);		
+	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {	
@@ -85,8 +100,8 @@ public class TwentyfortyeightGrid extends View {
 			int topLeftY = margin + (i / 4) * (margin + cellSize);
 			cell.set(topLeftX, topLeftY, topLeftX + cellSize, topLeftY + cellSize);
 			canvas.drawRoundRect(cell, 10.0f, 10.0f, emptyCellColor);
-			if (tiles[i] != 0) {
-				canvas.drawRoundRect(cell, 10.0f, 10.0f, tileColors[i]);
+			if (tiles[i] != 0) {				
+				canvas.drawRoundRect(cell, 10.0f, 10.0f, tileColors[tiles[i]]);
 			}
 
 		}
@@ -100,6 +115,77 @@ public class TwentyfortyeightGrid extends View {
 		int squareMeasure = MeasureSpec.makeMeasureSpec(size, MeasureSpec.EXACTLY);
 		super.onMeasure(squareMeasure, squareMeasure);
 	}
+
+	@Override
+	public void onChange(Engine e) {
+		tiles = e.getTiles();	
+		invalidate();
+	}
 	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		Log.d("GRID", "onTouchEvent");
+		if (gestureDetector == null) {
+			gestureDetector = new GestureDetectorCompat(getContext(), this);		
+		}
+		return gestureDetector.onTouchEvent(event);		
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		Log.d("GRID", "onFling");
+		if(velocityX > 0 && Math.abs(velocityX) > Math.abs(velocityY)) {
+			engine.right();
+		} else if (velocityX < 0 && Math.abs(velocityX) > Math.abs(velocityY)) {
+			engine.left();
+		} else if (velocityY > 0 && Math.abs(velocityY) > Math.abs(velocityX)) {
+			engine.down();
+		} else if (velocityY < 0 && Math.abs(velocityY) > Math.abs(velocityX)) {
+			engine.up();
+		}
+ 		return true;		
+	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		// NO-OP
+		return true;
+	}
+	
+	@Override
+	public void onLongPress(MotionEvent e) {
+		//NO-OP		
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		Log.d("GRID", "onScroll");
+//		float velocityX = distanceX;
+//		float velocityY = distanceY;
+//		if(velocityX > 0 && Math.abs(velocityX) > Math.abs(velocityY)) {
+//			engine.right();
+//		} else if (velocityX < 0 && Math.abs(velocityX) > Math.abs(velocityY)) {
+//			engine.left();
+//		} else if (velocityY > 0 && Math.abs(velocityY) > Math.abs(velocityX)) {
+//			engine.down();
+//		} else if (velocityY < 0 && Math.abs(velocityY) > Math.abs(velocityX)) {
+//			engine.up();
+//		}
+// 		return true;
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		// NO-OP		
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// NO-OP 
+		return false;
+	}	
 	
 }
